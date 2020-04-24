3cd98f4463fbd9c30ee71bf29b1c330343e59d10
ChuanShenLive <chuanshen@live.cn>
2020-04-24 13:40:53 +0800

    Update Login.vue

--- web-ui/src/views/Login.vue
+++ web-ui/src/views/Login.vue
@@ -1,13 +1,15 @@
 <template>
     <div>
-        <el-form :rules="rules" :model="loginForm">
-            <h3>系统登录</h3>
-            <el-from-item prop="username">
+        <el-form :model="loginForm" ref="loginForm" :rules="rules" class="loginContainer">
+            <h3 class="loginTitle">系统登录</h3>
+            <el-form-item prop="username">
                 <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入用户名"></el-input>
-            </el-from-item>
-            <el-from-item prop="password">
-                <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"></el-input>
-            </el-from-item>
+            </el-form-item>
+            <el-form-item prop="password">
+                <el-input auto-complete="off" placeholder="请输入密码" type="password" v-model="loginForm.password"></el-input>
+            </el-form-item>
+            <el-checkbox class="loginRemember" v-model="checked"></el-checkbox>
+            <el-button style="width: 100%;" type="primary" @click="submitLogin">登录</el-button>
         </el-form>
     </div>
 </template>
@@ -21,14 +23,49 @@
                     username: 'admin',
                     password: '123'
                 },
+                checked: true,
                 rules: {
                     username: [{required: true, message: '请输入用户名', trigger: 'blur'}],
                     password: [{required: true, message: '请输入密码', trigger: 'blur'}]
                 }
             }
+        },
+        methods: {
+            submitLogin() {
+                this.$refs.loginForm.validate((valid) => {
+                    if(valid) {
+                        alert('submit!');
+                    } else {
+                        this.$message.error('请输入所有字段');
+                        return false;
+                    }
+                });
+            }
         }
     }
 </script>
 
-<style scoped>
+<style>
+    .loginContainer {
+        border-radius: 15px;
+        background-clip: padding-box;
+        margin: 180px auto;
+        width: 350px;
+        padding: 15px 35px 15px 35px;
+        background: #fff;
+        border: 1px solid #eaeaea;
+        box-shadow: 0 0 25px #cac6c6;
+    }
+
+    .loginTitle {
+        margin: 15px auto 20px auto;
+        text-align: center;
+        color: #505458;
+    }
+
+    .loginRemember {
+        text-align: left;
+        margin: 0px 0px 15px 0px;
+    }
+
 </style>
