670e4ab34776f5d884a8a41b0273aa314a96339c
ChuanShenLive <chuanshen@live.cn>
2020-04-24 11:09:07 +0800

    2020-04-24

---
+++ web-ui/src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <el-form :rules="rules" :model="loginForm">
+            <h3>系统登录</h3>
+            <el-from-item prop="username">
+                <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入用户名"></el-input>
+            </el-from-item>
+            <el-from-item prop="password">
+                <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"></el-input>
+            </el-from-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data() {
+            return {
+                loginForm: {
+                    username: 'admin',
+                    password: '123'
+                },
+                rules: {
+                    username: [{required: true, message: '请输入用户名', trigger: 'blur'}],
+                    password: [{required: true, message: '请输入密码', trigger: 'blur'}]
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
