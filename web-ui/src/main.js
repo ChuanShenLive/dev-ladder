ea748a1355d32cf57c72bba7f969a69cae50b9e7
ChuanShenLive <chuanshen@live.cn>
2020-05-07 17:31:05 +0800

    2020-05-07

--- web-ui/src/main.js
+++ web-ui/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import store from './store';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
@@ -21,5 +22,6 @@ Vue.use(ElementUI);
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
