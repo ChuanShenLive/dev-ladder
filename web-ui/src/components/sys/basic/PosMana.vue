47336bf8471a320bf6b65495373fa32cf0174656
ChuanShenLive <chuanshen@live.cn>
2020-05-21 22:35:27 +0800

    2020-05-21

--- web-ui/src/components/sys/basic/PosMana.vue
+++ web-ui/src/components/sys/basic/PosMana.vue
@@ -1,12 +1,33 @@
 <template>
     <div>
-        职位管理
+        <div>
+            <el-input
+                placeholder="添加职位"
+                prefix-icon="el-icon-plus"
+                size="small"
+                style="width: 300px; margin-right: 8px"
+                v-model="pos.name">
+            </el-input>
+
+            <el-button icon="el-icon-plus" size="small" type="primary">添加</el-button>
+        </div>
+        <div>
+            <el-table
+                :data=""
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "PosMana"
+        name: "PosMana",
+        data() {
+            return {
+                pos: {
+                    name: ''
+                }
+            }
+        }
     }
 </script>
 
