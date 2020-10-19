1cb7dd07bfa1ad96113ee2f2c08b36d16f7f242c
ChuanShen <chuanshen@live.cn>
2020-10-19 14:13:26 +0800

    2020-10-19

--- server/src/main/java/org/chuanshen/devladder/config/SecurityConfig.java
+++ server/src/main/java/org/chuanshen/devladder/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     CustomUrlDecisionManager customUrlDecisionManager;
 
     @Bean
-    PasswordEncoder passwordEncoder () {
+    PasswordEncoder passwordEncoder() {
         // return PasswordEncoderFactories.createDelegatingPasswordEncoder();
         return new BCryptPasswordEncoder();
     }
@@ -68,84 +68,84 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .withObjectPostProcessor(new ObjectPostProcessor<FilterSecurityInterceptor>() {
-                        @Override
-                        public <O extends FilterSecurityInterceptor> O postProcess(O object) {
-                            object.setAccessDecisionManager(customUrlDecisionManager);
-                            object.setSecurityMetadataSource(customFilterInvocationSecurityMetadataSource);
-                            return object;
-                        }
-                    })
-                    .anyRequest().authenticated()
-                    .and()
-                .formLogin()
-                    .usernameParameter("username").passwordParameter("password").loginProcessingUrl("/doLogin").loginPage("/login")
-                    .successHandler(new AuthenticationSuccessHandler() {
-                        @Override
-                        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                            response.setContentType("application/json;charset=utf-8");
-                            PrintWriter out = response.getWriter();
-                            Hr hr = (Hr) authentication.getPrincipal();
-                            hr.setPassword(null);
-                            RespBean ok = RespBean.ok("登录成功!", hr);
-                            String s = new ObjectMapper().writeValueAsString(ok);
-                            out.write(s);
-                            out.flush();
-                            out.close();
-                        }
-                    })
-                    .failureHandler(new AuthenticationFailureHandler() {
-                        @Override
-                        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-                            response.setContentType("application/json;charset=utf-8");
-                            PrintWriter out = response.getWriter();
-                            RespBean respBean = RespBean.error("登录失败!");
-                            if (exception instanceof LockedException) {
-                                respBean.setMsg("账户被锁定, 请联系管理员!");
-                            } else if (exception instanceof CredentialsExpiredException) {
-                                respBean.setMsg("密码过期, 请联系管理员!");
-                            } else if (exception instanceof AccountExpiredException) {
-                                respBean.setMsg("账户过期, 请联系管理员");
-                            } else if (exception instanceof DisabledException) {
-                                respBean.setMsg("账户被禁用, 请联系管理员!");
-                            } else if (exception instanceof BadCredentialsException) {
-                                respBean.setMsg("用户名或者密码输入错误, 请重新输入!");
-                            }
-                            out.write(new ObjectMapper().writeValueAsString(respBean));
-                            out.flush();
-                            out.close();
-                        }
-                    })
-                    .permitAll()
-                    .and()
-                .logout()
-                    .logoutSuccessHandler(new LogoutSuccessHandler() {
-                        @Override
-                        public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                            response.setContentType("application/json;charset=utf-8");
-                            PrintWriter out = response.getWriter();
-                            out.write(new ObjectMapper().writeValueAsString(RespBean.ok("注销成功!")));
-                            out.flush();
-                            out.close();
-                        }
-                    })
-                    .permitAll()
-                    .and()
-                .csrf().disable()
-                .exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {  // 没有认证时, 在此处处理, 不要重定向
+                .withObjectPostProcessor(new ObjectPostProcessor<FilterSecurityInterceptor>() {
                     @Override
-                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                    public <O extends FilterSecurityInterceptor> O postProcess(O object) {
+                        object.setAccessDecisionManager(customUrlDecisionManager);
+                        object.setSecurityMetadataSource(customFilterInvocationSecurityMetadataSource);
+                        return object;
+                    }
+                })
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .usernameParameter("username").passwordParameter("password").loginProcessingUrl("/doLogin").loginPage("/login")
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
                         response.setContentType("application/json;charset=utf-8");
-                        response.setStatus(401);
                         PrintWriter out = response.getWriter();
-                        RespBean respBean = RespBean.error("访问失败!");
-                        if (authException instanceof InsufficientAuthenticationException) {
-                            respBean.setMsg("非法请求失败, 请联系管理员!");
+                        Hr hr = (Hr) authentication.getPrincipal();
+                        hr.setPassword(null);
+                        RespBean ok = RespBean.ok("登录成功!", hr);
+                        String s = new ObjectMapper().writeValueAsString(ok);
+                        out.write(s);
+                        out.flush();
+                        out.close();
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                        response.setContentType("application/json;charset=utf-8");
+                        PrintWriter out = response.getWriter();
+                        RespBean respBean = RespBean.error("登录失败!");
+                        if (exception instanceof LockedException) {
+                            respBean.setMsg("账户被锁定, 请联系管理员!");
+                        } else if (exception instanceof CredentialsExpiredException) {
+                            respBean.setMsg("密码过期, 请联系管理员!");
+                        } else if (exception instanceof AccountExpiredException) {
+                            respBean.setMsg("账户过期, 请联系管理员");
+                        } else if (exception instanceof DisabledException) {
+                            respBean.setMsg("账户被禁用, 请联系管理员!");
+                        } else if (exception instanceof BadCredentialsException) {
+                            respBean.setMsg("用户名或者密码输入错误, 请重新输入!");
                         }
                         out.write(new ObjectMapper().writeValueAsString(respBean));
                         out.flush();
                         out.close();
                     }
-                });
+                })
+                .permitAll()
+                .and()
+                .logout()
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.setContentType("application/json;charset=utf-8");
+                        PrintWriter out = response.getWriter();
+                        out.write(new ObjectMapper().writeValueAsString(RespBean.ok("注销成功!")));
+                        out.flush();
+                        out.close();
+                    }
+                })
+                .permitAll()
+                .and()
+                .csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {  // 没有认证时, 在此处处理, 不要重定向
+            @Override
+            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                response.setContentType("application/json;charset=utf-8");
+                response.setStatus(401);
+                PrintWriter out = response.getWriter();
+                RespBean respBean = RespBean.error("访问失败!");
+                if (authException instanceof InsufficientAuthenticationException) {
+                    respBean.setMsg("非法请求失败, 请联系管理员!");
+                }
+                out.write(new ObjectMapper().writeValueAsString(respBean));
+                out.flush();
+                out.close();
+            }
+        });
     }
 }
--- server/src/test/java/org/chuanshen/devladder/DevLadderServerApplicationTests.java
+++ server/src/test/java/org/chuanshen/devladder/DevLadderServerApplicationTests.java
@@ -1,13 +1,23 @@
 package org.chuanshen.devladder;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class DevLadderServerApplicationTests {
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void getPassword() {
+        System.out.println(passwordEncoder.encode("123"));
     }
 
 }
